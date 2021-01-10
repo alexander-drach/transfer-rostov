6a5cdee2b01b0e3c0fab74d85dfbc8a89d189e79
Elena Slyusareva <sportgirl55@bk.ru>
2021-01-11 02:38:35 +0500

    structure of booking form is done

---
+++ page-about.php
@@ -0,0 +1,42 @@
+<main class="page">
+  <h2 class="title-section">О нас</h2>
+  <section class="about">
+    <div class="about-item services">
+      <p class="item-title">Услуги междугороднего такси и трансфера</p>
+      <p class="about-text">
+        Наша команда профессионалов начала свою работу
+        в 2017 году с организации пассажирских перевозок
+        из города Ростов-на-Дону по области.
+
+        На тот момент в нашем автопарке насчитывалось
+        около 10 автомобилей.
+
+        Сейчас мы работаем по всему Югу России
+        и республике Крым, в Волгоградской,
+        Астраханской и других областях, Калмыкии,
+        поездки на границу с Украиной (таможня), по РФ.</p>
+
+    </div>
+    <div class="about-item methods">
+      <p class="item-title">Забронировать автомобиль можете любым удобным для Вас способом:</p>
+
+    </div>
+
+  </section>
+  <section class="transfer">
+    <p class="item-title">Трансфер Аэропорт “Платов”</p>
+    <div class="wrapper">
+      <div class="item transfer-text">
+
+      </div>
+      <div class="item transfer-img">
+
+      </div>
+
+    </div>
+  </section>
+  <section class="feedback">
+    <h2 class="title-section">Отзывы наших пассажиров</h2>
+
+  </section>
+</main>
--- page-booking.php
+++ page-booking.php
@@ -1,53 +1,135 @@
 <main class="page">
-  <h2 class="title-section">Заполните форму</h2>
-  <form action="">
-    <fieldset>
-      <label for="name">
-        <input id="name" type="text" name="name" placeholder="Ваше имя*" required="">
+  <span class="title-page">Заполните форму</span>
+  <form class="form form-booking">
+
+    <div class="field">
+      <label class="label" for="name">
+        <span>Ваше имя</span>
+        <input class="input" id="name" type="text" name="name" placeholder="Ваше имя*" required="">
+      </label>
+    </div>
+
+    <div class="field">
+      <label class="label" for="phone">
+        <span>Телефон*</span>
+        <input class="input" id="tel" type="tel" name="phone" placeholder="Телефон*" required="">
+      </label>
+    </div>
+
+    <div class="field">
+      <label class="label" for="email">
+        <span>Ваш email</span>
+        <input class="input" id="email" type="email" name="email" placeholder="Ваш email">
+      </label>
+    </div>
+
+    <div class="field">
+      <label class="label" for="info">
+        <span>Дополнительная информация*</span>
+        <textarea id="info" name="info">Дополнительная информация*</textarea>
+      </label>
+    </div>
+
+    <div class="field field-group">
+      <label class="label" for="where">
+        <span>Откуда*</span>
+        <input class="input" id="where" type="text" name="where" placeholder="Откуда*" required="">
       </label>
 
-      <label for="phone">
-        <input id="tel" type="tel" name="phone" placeholder="Телефон*" required="">
+      <label class="label" for="somewhere">
+        <span>Куда*</span>
+        <input class="input" id="somewhere" type="text" name="somewhere" placeholder="Куда*" required="">
+      </label>
+    </div>
+
+    <div class="field field-group">
+      <label class="label" for="date">
+        <span>Дата*</span>
+        <input class="input" id="date" type="date" name="date" placeholder="Дата*" required="">
       </label>
 
-      <label for="email">
-        <input id="email" type="email" name="email" placeholder="Ваш email">
+      <label class="label" for="time">
+        <span>Время*</span>
+        <input class="input" id="time" type="time" name="time" placeholder="Время*" required="">
+      </label>
+    </div>
+
+    <div class="field field-group">
+      <label class="label" for="passengers">
+        <span>Количество пассажиров*</span>
+        <input class="input" id="passengers" type="number" name="passengers" placeholder="Количество пассажиров*"
+          required="">
       </label>
 
-      <label for="info">
-        <input id="info" type="text" name="info" placeholder="Дополнительная информация*" required="">
+      <label class="label" for="children">
+        <span>Количество детей*</span>
+        <input class="input" id="children" type="number" name="children" placeholder="Количество детей*" required="">
       </label>
+    </div>
 
-      <label for="where">
-        <input id="where" type="text" name="where" placeholder="Откуда*" required="">
-      </label>
-
-      <label for="somewhere">
-        <input id="somewhere" type="text" name="somewhere" placeholder="Куда*" required="">
-      </label>
-
-      <label for="time">
-        <input id="time" type="datetime-local" name="time" placeholder="Дата / время*" required="">
-      </label>
-
-      <label for="passengers">
-        <input id="passengers" type="number" name="passengers" placeholder="Количество пассажиров*" required="">
-      </label>
-
-      <label for="children">
-        <input id="children" type="number" name="children" placeholder="Количество детей*" required="">
-      </label>
-
-      <label for="luggage">
+    <div class="radio">
+      <div class="radio-group">
         <span>Крупногабаритный багаж*</span>
-        <span><input id="luggage" type="radio" name="radio-item" value="yes">Да</span>
-        <span><input id="luggage" type="radio" name="radio-item" value="no">Нет</span>
-      </label>
 
-      <label for="rate">
-        <input id="rate" type="text" name="rate" placeholder="Выберите тариф*" required="">
+        <input class="radio-button" id="luggage1" type="radio" name="luggage" value="yes">
+        <label class="label" for="luggage1">Да</label>
+
+        <input class="radio-button" id="luggage2" type="radio" name="luggage" value="no">
+        <label class="label" for="luggage2">Нет</label>
+      </div>
+
+
+      <div class="radio-group">
+        <span>Оборудование для перевозки детей*</span>
+
+        <input class="radio-button" id="equipment1" type="radio" name="equipment" value="yes">
+        <label class="label" for="equipment1">Люлька</label>
+
+        <input class="radio-button" id="equipment2" type="radio" name="equipment" value="no">
+        <label class="label" for="equipment2">Кресло</label>
+
+        <input class="radio-button" id="equipment3" type="radio" name="equipment" value="yes">
+        <label class="label" for="equipment3">Адаптер для ремня</label>
+
+        <input class="radio-button" id="equipment4" type="radio" name="equipment" value="yes">
+        <label class="label" for="equipment4">Нет (12+)</label>
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label" for="rate">
+        <span>Категория транспорта*</span>
+        <select name="rate" id="rate">
+          <option value="Стандарт">Стандарт</option>
+          <option value="Стандарт">Комфорт</option>
+          <option value="Стандарт">Бизнес</option>
+        </select>
+        <!-- <input class="input" id="rate" type="text" name="rate" placeholder="Выберите тариф*" required=""> -->
       </label>
-      
-    </fieldset>
+    </div>
+
+    <div class="radio">
+      <div class="radio-group">
+        <span>Квитанция о поездке*</span>
+
+        <input class="radio-button" id="receipt1" type="radio" name="receipt" value="no">
+        <label class="label" for="receipt1">Нет</label>
+
+        <input class="radio-button" id="receipt2" type="radio" name="receipt" value="yes">
+        <label class="label" for="receipt2">Нужен БСО</label>
+      </div>
+
+      <div class="radio-group">
+        <span>Встреча с табличкой*</span>
+
+        <input class="radio-button" id="tablet1" type="radio" name="tablet" value="yes">
+        <label class="label" for="tablet1">Да</label>
+
+        <input class="radio-button" id="tablet2" type="radio" name="tablet" value="no">
+        <label class="label" for="tablet2">Нет</label>
+
+      </div>
+    </div>
+    <button class="order-btn" type="submit">Забронировать</button>
   </form>
 </main>

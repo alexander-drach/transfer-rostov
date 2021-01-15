cc54102e560b2ab9eb3c7bbc223c4bc1b1f7cfd2
Elena Slyusareva <sportgirl55@bk.ru>
2021-01-15 22:33:49 +0500

    Continue to make out the form

--- index.php
+++ index.php
@@ -106,6 +106,7 @@
       <h2 class="title-section">Популярные направления</h2>
       <div class="container">
         <ul class="list list_routs">
+
           <?php foreach ($routs as $item) : ?>
           <li class="list__item">
             <img class="routs__img" src="<?php echo $item['img']; ?>" alt="">
@@ -113,11 +114,14 @@
             <a class="routs__link" href="./page-booking.php?title=<?php echo $item['title']; ?>">Заказать</a>
             <div class="routs__overlay"></div>
           </li><!-- list__item -->
+
           <?php endforeach; ?>
+          
         </ul>
       </div>
     </section>
 
   </main>
+  <?php include 'footer.php'?>
 
 </html>
